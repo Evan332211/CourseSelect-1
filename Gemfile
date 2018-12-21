cbc40f04672fcc58887ca2544ac207b4826c82aa
David <heshi@cug.edu.cn>
2018-12-21 14:55:29 +0800

    separating database and app succeed!

--- Gemfile
+++ Gemfile
@@ -41,7 +41,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # for postgresql
-gem 'pg'
+# gem 'pg'
+# gem 'pg', '~> 0.20'
 
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -55,6 +56,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   # Use sqlite3 as the database for Active Record
+  # gem 'pg'
 end
 
 group :development do
@@ -62,10 +64,13 @@ group :development do
   gem 'web-console', '~> 2.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
 end
 
 group :production do
   gem 'rails_12factor'
+  # gem 'pg'
+  gem 'mysql2', '~>0.4.0'
 end
 gem 'puma'
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,126 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20170103050207) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.integer  "item_one"
-    t.integer  "item_two"
-    t.integer  "item_three"
-    t.integer  "item_four"
-    t.integer  "item_five"
-    t.integer  "item_six"
-    t.integer  "item_seven"
-    t.integer  "item_eight"
-    t.integer  "item_nine"
-    t.integer  "item_ten"
-    t.integer  "item_eleven"
-    t.integer  "item_twelve"
-    t.integer  "item_thirteen"
-    t.integer  "item_fourteen"
-    t.integer  "item_fifteen"
-    t.integer  "item_sixteen"
-    t.integer  "item_seventeen"
-    t.text     "comment_advantage"
-    t.text     "comment_disadvantage"
-    t.boolean  "commented",            default: false
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-  end
-
-  add_index "comments", ["course_id"], name: "index_comments_on_course_id", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "courses", force: :cascade do |t|
-    t.string   "name"
-    t.string   "course_code"
-    t.string   "course_type"
-    t.string   "teaching_type"
-    t.string   "exam_type"
-    t.string   "credit"
-    t.integer  "limit_num"
-    t.integer  "student_num",         default: 0
-    t.string   "class_room"
-    t.string   "course_time"
-    t.string   "course_week"
-    t.string   "course_difficulty"
-    t.string   "course_suit"
-    t.string   "course_score"
-    t.text     "course_outline"
-    t.text     "course_exam_details"
-    t.text     "course_chapter"
-    t.string   "course_live"
-    t.string   "course_homework"
-    t.string   "course_teamwork"
-    t.integer  "semester_id"
-    t.integer  "teacher_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "open",                default: false
-  end
-
-  add_index "courses", ["semester_id"], name: "index_courses_on_semester_id", using: :btree
-
-  create_table "grades", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.integer  "grade"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "open",       default: false
-  end
-
-  add_index "grades", ["course_id"], name: "index_grades_on_course_id", using: :btree
-  add_index "grades", ["user_id"], name: "index_grades_on_user_id", using: :btree
-
-  create_table "notices", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "semesters", force: :cascade do |t|
-    t.string   "info",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "num"
-    t.string   "major"
-    t.string   "department"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.boolean  "teacher",         default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -136,111 +13,107 @@ end
 
 ActiveRecord::Schema.define(version: 20170107051134) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.integer  "item_one"
-    t.integer  "item_two"
-    t.integer  "item_three"
-    t.integer  "item_four"
-    t.integer  "item_five"
-    t.integer  "item_six"
-    t.integer  "item_seven"
-    t.integer  "item_eight"
-    t.integer  "item_nine"
-    t.integer  "item_ten"
-    t.integer  "item_eleven"
-    t.integer  "item_twelve"
-    t.integer  "item_thirteen"
-    t.integer  "item_fourteen"
-    t.integer  "item_fifteen"
-    t.integer  "item_sixteen"
-    t.integer  "item_seventeen"
-    t.text     "comment_advantage"
-    t.text     "comment_disadvantage"
-    t.boolean  "commented",            default: false
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.integer  "course_id",            limit: 4
+    t.integer  "user_id",              limit: 4
+    t.integer  "item_one",             limit: 4
+    t.integer  "item_two",             limit: 4
+    t.integer  "item_three",           limit: 4
+    t.integer  "item_four",            limit: 4
+    t.integer  "item_five",            limit: 4
+    t.integer  "item_six",             limit: 4
+    t.integer  "item_seven",           limit: 4
+    t.integer  "item_eight",           limit: 4
+    t.integer  "item_nine",            limit: 4
+    t.integer  "item_ten",             limit: 4
+    t.integer  "item_eleven",          limit: 4
+    t.integer  "item_twelve",          limit: 4
+    t.integer  "item_thirteen",        limit: 4
+    t.integer  "item_fourteen",        limit: 4
+    t.integer  "item_fifteen",         limit: 4
+    t.integer  "item_sixteen",         limit: 4
+    t.integer  "item_seventeen",       limit: 4
+    t.text     "comment_advantage",    limit: 65535
+    t.text     "comment_disadvantage", limit: 65535
+    t.boolean  "commented",                          default: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
   end
 
   add_index "comments", ["course_id"], name: "index_comments_on_course_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name"
-    t.string   "course_code"
-    t.string   "course_type"
-    t.string   "teaching_type"
-    t.string   "exam_type"
-    t.string   "credit"
-    t.integer  "limit_num"
-    t.integer  "student_num",         default: 0
-    t.string   "class_room"
-    t.string   "course_time"
-    t.string   "course_week"
-    t.string   "course_difficulty"
-    t.string   "course_suit"
-    t.string   "course_score"
-    t.text     "course_outline"
-    t.text     "course_exam_details"
-    t.text     "course_chapter"
-    t.string   "course_live"
-    t.string   "course_homework"
-    t.string   "course_teamwork"
-    t.integer  "semester_id"
-    t.integer  "teacher_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "open",                default: false
+    t.string   "name",                limit: 255
+    t.string   "course_code",         limit: 255
+    t.string   "course_type",         limit: 255
+    t.string   "teaching_type",       limit: 255
+    t.string   "exam_type",           limit: 255
+    t.string   "credit",              limit: 255
+    t.integer  "limit_num",           limit: 4
+    t.integer  "student_num",         limit: 4,     default: 0
+    t.string   "class_room",          limit: 255
+    t.string   "course_time",         limit: 255
+    t.string   "course_week",         limit: 255
+    t.string   "course_difficulty",   limit: 255
+    t.string   "course_suit",         limit: 255
+    t.string   "course_score",        limit: 255
+    t.text     "course_outline",      limit: 65535
+    t.text     "course_exam_details", limit: 65535
+    t.text     "course_chapter",      limit: 65535
+    t.string   "course_live",         limit: 255
+    t.string   "course_homework",     limit: 255
+    t.string   "course_teamwork",     limit: 255
+    t.integer  "semester_id",         limit: 4
+    t.integer  "teacher_id",          limit: 4
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.boolean  "open",                              default: false
   end
 
   add_index "courses", ["semester_id"], name: "index_courses_on_semester_id", using: :btree
 
   create_table "grades", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.integer  "grade"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "open",       default: false
+    t.integer  "course_id",  limit: 4
+    t.integer  "user_id",    limit: 4
+    t.integer  "grade",      limit: 4
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.boolean  "open",                 default: false
   end
 
   add_index "grades", ["course_id"], name: "index_grades_on_course_id", using: :btree
   add_index "grades", ["user_id"], name: "index_grades_on_user_id", using: :btree
 
   create_table "notices", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",      limit: 255
+    t.text     "text",       limit: 65535
+    t.string   "department", limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "semesters", force: :cascade do |t|
-    t.string   "info",                       null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "is_open",    default: false
+    t.string   "info",       limit: 255,                 null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "is_open",                default: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "num"
-    t.string   "major"
-    t.string   "department"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.boolean  "teacher",         default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.string   "num",             limit: 255
+    t.string   "major",           limit: 255
+    t.string   "department",      limit: 255
+    t.string   "password_digest", limit: 255
+    t.string   "remember_digest", limit: 255
+    t.boolean  "admin",                       default: false
+    t.boolean  "teacher",                     default: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
->>>>>>> 0f0daea3201dc1e0660993f7c7ba104f93eb684e
